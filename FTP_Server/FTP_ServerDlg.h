a01df91916cebe8bcc3598723289314c4b7ec01f
漫漫变黑 <@-PC>
2020-03-18 21:09:16 +0800

    添加项目文件。

---
+++ FTP_Server/FTP_ServerDlg.h
@@ -0,0 +1,33 @@
+
+// FTP_ServerDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CFTP_ServerDlg 对话框
+class CFTP_ServerDlg : public CDialogEx
+{
+// 构造
+public:
+	CFTP_ServerDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FTP_SERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
